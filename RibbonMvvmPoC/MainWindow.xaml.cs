84a614bd0c30786bbc4e12fd9de78c54cda3c1c7
Marcin <Marcin@C-ADVENTURE-HQ>
2017-07-03 22:46:33 +0200

    Add project files.

---
+++ RibbonMvvmPoC/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace RibbonMvvmPoC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Messenger.Default.Register<string>(this, ShowWindow);
+        }
+
+        private void ShowWindow(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private void RibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            // we don't like events anymore ;-)
+            MessageBox.Show("Old way event!");
+        }
+    }
+}
---
+++ RibbonMvvmPoC/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace RibbonMvvmPoC.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private int _clickCounter;
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            DoSomethingCommand = new RelayCommand(DoSomething, () => true);
+            IncrementCounterCommand = new RelayCommand(IncrementCounter, () => true);
+            ClickCounter = 0;
+        }
+
+
+        public RelayCommand DoSomethingCommand { get; private set; }
+        public RelayCommand IncrementCounterCommand { get; private set; }
+
+        public int ClickCounter
+        {
+            get { return _clickCounter; }
+            set
+            {
+                _clickCounter = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void IncrementCounter()
+        {
+            ClickCounter += 1;
+        }
+
+        private void DoSomething()
+        {
+            MessengerInstance.Send("New way message!");
+        }
+    }
+}
